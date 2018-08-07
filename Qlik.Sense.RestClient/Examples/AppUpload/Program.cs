fce66a00017e522868f42eb98805e060eb4f42df
Øystein Kolsrud <yko@qlik.com>
2018-08-07 08:35:19 +0200

    Added AppUpload example.

---
+++ Qlik.Sense.RestClient/Examples/AppUpload/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Qlik.Sense.RestClient;
+
+namespace AppUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var senseServerUrl = args.Any() ? args[0] : "https://my.server.url";
+            var restClient = new RestClient(senseServerUrl);
+            restClient.AsNtlmUserViaProxy();
+
+            var data = File.ReadAllBytes(@"\path\to\app.qvf");
+            const string nameOfApp = "MyUploadedApp";
+            using (new WebRequestSetting(restClient, req => req.ContentType = "application/vnd.qlik.sense.app"))
+                Console.WriteLine(restClient.Post("qrs/app/upload?keepData=true&name=" + nameOfApp, data));
+        }
+    }
+}
