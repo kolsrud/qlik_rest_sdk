0e8324069e153a66f9092faf5eccc966add2a45c
Øystein Kolsrud <yko@qlik.com>
2018-10-08 15:51:55 +0200

    Added CreateStream example.

---
+++ Qlik.Sense.RestClient/Examples/CreateStream/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Qlik.Sense.RestClient;
+
+namespace CreateStream
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var senseServerUrl = args.Any() ? args[0] : "https://my.server.url";
+			var restClient = new RestClient(senseServerUrl);
+			restClient.AsNtlmUserViaProxy();
+
+			Console.WriteLine(restClient.Post("qrs/stream", "{ name : \"MyStream\" }"));
+		}
+	}
+}
