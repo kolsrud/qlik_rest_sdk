7981ea06be195dc4b34622b7d83b96f966829f7f
Øystein Kolsrud <yko@qlik.com>
2018-10-23 09:17:05 +0200

    Added PUT example.

---
+++ Qlik.Sense.RestClient/Examples/PublishApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qlik.Sense.RestClient;
+
+namespace PublishApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var senseServerUrl = args.Any() ? args[0] : "https://my.server.url";
+            var restClient = new RestClient(senseServerUrl);
+            restClient.AsNtlmUserViaProxy();
+            var appId = "app-identifer";
+            var streamId = "stream-identifier";
+            using (new DebugConsole())
+                restClient.Put(string.Format("/qrs/app/{0}/publish?stream={1}", appId, streamId), "");
+        }
+    }
+}
