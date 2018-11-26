1c7b4014deaac69d66df3f71ec0f17266ee7064e
Øystein Kolsrud <yko@qlik.com>
2018-11-26 12:41:56 +0100

    Moved classes to separate files.

---
+++ Qlik.Sense.RestClient/ConnectionSettings.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Qlik.Sense.RestClient
+{
+    internal class ConnectionSettings : IConnectionConfigurator
+    {
+        public Uri BaseUri { get; set; }
+
+        public CookieContainer CookieJar { get; set; }
+
+        private bool _isConfigured = false;
+        public RestClient.ConnectionType ConnectionType;
+        public string UserDirectory;
+        public string UserId;
+        public string StaticHeaderName;
+        public bool UseDefaultCredentials;
+        public X509Certificate2Collection Certificates;
+        public Action<HttpWebRequest> WebRequestTransform { get; set; }
+
+        public bool HasCookie => CookieJar.Count > 0;
+
+        public ConnectionSettings Clone()
+        {
+            return new ConnectionSettings()
+            {
+                BaseUri = this.BaseUri,
+                CookieJar = this.CookieJar,
+                _isConfigured = this._isConfigured,
+                ConnectionType = this.ConnectionType,
+                UserDirectory = this.UserDirectory,
+                UserId = this.UserId,
+                StaticHeaderName = this.StaticHeaderName,
+                UseDefaultCredentials = this.UseDefaultCredentials,
+                Certificates = this.Certificates,
+                WebRequestTransform = this.WebRequestTransform
+            };
+        }
+
+        private ConnectionSettings()
+        {
+            CookieJar = new CookieContainer();
+        }
+
+        public ConnectionSettings(string uri) : this()
+        {
+            BaseUri = new Uri(uri);
+        }
+
+        public void AsDirectConnection(int port = 4242, bool certificateValidation = true,
+            X509Certificate2Collection certificateCollection = null)
+        {
+            AsDirectConnection(Environment.UserName, Environment.UserDomainName, port, certificateValidation,
+                certificateCollection);
+        }
+
+        public void AsDirectConnection(string userDirectory, string userId, int port = 4242,
+            bool certificateValidation = true, X509Certificate2Collection certificateCollection = null)
+        {
+            ConnectionType = RestClient.ConnectionType.DirectConnection;
+            var uriBuilder = new UriBuilder(BaseUri) {Port = port};
+            BaseUri = uriBuilder.Uri;
+            UserId = userId;
+            UserDirectory = userDirectory;
+            Certificates = certificateCollection;
+            if (!certificateValidation)
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            _isConfigured = true;
+        }
+
+        public void AsNtlmUserViaProxy(bool certificateValidation = true)
+        {
+            UseDefaultCredentials = true;
+            ConnectionType = RestClient.ConnectionType.NtlmUserViaProxy;
+            UserId = Environment.UserName;
+            UserDirectory = Environment.UserDomainName;
+            if (!certificateValidation)
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            _isConfigured = true;
+        }
+
+        public void AsStaticHeaderUserViaProxy(string userId, string headerName, bool certificateValidation = true)
+        {
+            ConnectionType = RestClient.ConnectionType.StaticHeaderUserViaProxy;
+            UserId = userId;
+            UserDirectory = Environment.UserDomainName;
+            StaticHeaderName = headerName;
+            if (!certificateValidation)
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            _isConfigured = true;
+        }
+
+        public void Validate()
+        {
+            if (!_isConfigured)
+                throw new RestClient.ConnectionNotConfiguredException();
+            if (ConnectionType == RestClient.ConnectionType.DirectConnection && Certificates == null)
+                throw new RestClient.CertificatesNotLoadedException();
+        }
+    }
+}
---
+++ Qlik.Sense.RestClient/SenseWebClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Qlik.Sense.RestClient
+{
+    internal class SenseWebClient : WebClient
+    {
+        private readonly ConnectionSettings _connectionSettings;
+
+        public SenseWebClient(ConnectionSettings settings)
+        {
+            _connectionSettings = settings;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var xrfkey = CreateXrfKey();
+            var request = (HttpWebRequest) base.GetWebRequest(AddXrefKey(address, xrfkey));
+            request.ContentType = "application/json";
+            request.Headers.Add("X-Qlik-Xrfkey", xrfkey);
+            _connectionSettings.WebRequestTransform?.Invoke(request);
+
+            var userHeaderValue = string.Format("UserDirectory={0};UserId={1}", _connectionSettings.UserDirectory,
+                _connectionSettings.UserId);
+            switch (_connectionSettings.ConnectionType)
+            {
+                case RestClient.ConnectionType.NtlmUserViaProxy:
+                    request.UseDefaultCredentials = true;
+                    request.AllowAutoRedirect = true;
+                    request.UserAgent = "Windows";
+                    break;
+                case RestClient.ConnectionType.DirectConnection:
+                    request.Headers.Add("X-Qlik-User", userHeaderValue);
+                    foreach (var certificate in _connectionSettings.Certificates)
+                    {
+                        request.ClientCertificates.Add(certificate);
+                    }
+
+                    break;
+                case RestClient.ConnectionType.StaticHeaderUserViaProxy:
+                    request.Headers.Add(_connectionSettings.StaticHeaderName, _connectionSettings.UserId);
+                    break;
+            }
+
+            request.CookieContainer = _connectionSettings.CookieJar;
+            return request;
+        }
+
+        private static Uri AddXrefKey(Uri uri, string xrfkey)
+        {
+            var sb = new StringBuilder(uri.Query.TrimStart('?'));
+            if (sb.Length > 0)
+                sb.Append('&');
+            sb.Append("xrfkey=" + xrfkey);
+            var uriBuilder = new UriBuilder(uri) {Query = sb.ToString()};
+            return uriBuilder.Uri;
+        }
+
+        private static string CreateXrfKey()
+        {
+            const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            var sb = new StringBuilder(16);
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                var randomSequence = new byte[16];
+                provider.GetBytes(randomSequence);
+                foreach (var b in randomSequence)
+                {
+                    var character = allowedChars[b % allowedChars.Length];
+                    sb.Append(character);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
