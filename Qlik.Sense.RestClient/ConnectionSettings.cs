831fc8df5808a65d1f83dfdf5c3db41dc0219c23
Øystein Kolsrud <yko@qlik.com>
2020-09-12 13:44:34 +0200

    Removed redundant using clause.

--- Qlik.Sense.RestClient/ConnectionSettings.cs
+++ Qlik.Sense.RestClient/ConnectionSettings.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Threading;
