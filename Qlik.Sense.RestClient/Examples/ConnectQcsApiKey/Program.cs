d5c10eded69dd0aa1828132dd3a2ff26a7c8b9a3
Øystein Kolsrud <yko@qlik.com>
2022-05-02 13:56:06 +0200

    Added examples `ConnectQcsApiKey` and `ConnectQcsJwt`.

---
+++ Qlik.Sense.RestClient/Examples/ConnectQcsApiKey/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Qlik.Sense.RestClient;
+
+namespace ConnectQcsApiKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var apiKey = "<apiKey>";
+
+            var restClient = new RestClient(url);
+            restClient.AsApiKeyViaQcs(apiKey);
+
+            Console.WriteLine(restClient.Get<JToken>("/api/v1/users/me"));
+        }
+    }
+}
---
+++ Qlik.Sense.RestClient/Examples/ConnectQcsJwt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Qlik.Sense.Jwt;
+using Qlik.Sense.RestClient;
+
+namespace ConnectQcsJwt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var privateKeyPath = @"C:\path\to\privatekey.pem";
+            var keyId = "<keyId>";
+            var issuer = "<issuer>";
+            var subject = "<subjectID>";
+            var name = "<userName>";
+
+            var jwtFactory = new QcsJwtFactory(privateKeyPath, keyId, issuer);
+            var jwt = jwtFactory.MakeJwt(subject, name);
+
+            var restClient = new RestClient(url);
+            restClient.AsJsonWebTokenViaQcs(jwt);
+
+            Console.WriteLine(restClient.Get<JToken>("/api/v1/users/me"));
+        }
+    }
+}
