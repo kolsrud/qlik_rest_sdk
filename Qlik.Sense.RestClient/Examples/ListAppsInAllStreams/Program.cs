a4c1afccf0ab4227d0c382cdf535b336fd26ed31
Øystein Kolsrud <yko@qlik.com>
2022-06-03 08:52:15 +0200

    Added example or using filters to list apps in streams.

---
+++ Qlik.Sense.RestClient/Examples/ListAppsInAllStreams/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Qlik.Sense.RestClient;
+
+namespace ListAppsInAllStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var senseServerUrl = args.Any() ? args[0] : "https://my.server.url";
+            var restClient = new RestClient(senseServerUrl);
+            restClient.AsNtlmUserViaProxy();
+            foreach (var stream in restClient.Get<JArray>("/qrs/stream"))
+            {
+                Console.WriteLine($"Apps in stream: {stream["id"]}: {stream["name"]}");
+                foreach (var app in restClient.Get<JArray>($"/qrs/app?filter=stream.id eq {stream["id"]}"))
+                {
+                    Console.WriteLine($"  {app["id"]}: {app["name"]}");
+                }
+            }
+        }
+    }
+}
