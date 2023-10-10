225f211fa20d69042e8528df22cc299ca3e48b26
Øystein Kolsrud <yko@qlik.com>
2023-10-10 17:31:19 +0200

    Cosmetic changes only.

--- Qlik.Sense.RestClient/Examples/QcsUploadDataFile/Program.cs
+++ Qlik.Sense.RestClient/Examples/QcsUploadDataFile/Program.cs
@@ -8,19 +8,20 @@ namespace QcsUploadDataFile
     {
         static void Main(string[] args)
         {
-            var url = "https://yko.eu.qlikcloud.com/";
-            var apiKey = "eyJhbGciOiJFUzM4NCIsImtpZCI6IjVlNzM4Yjg5LWZmMmQtNGFiNC1hN2QyLTdlOTgzNDI0OGUyMSIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiYWNaNGdFUE1nd3lOZm1lV0lpeElpWTNCY29FejllZjciLCJqdGkiOiI1ZTczOGI4OS1mZjJkLTRhYjQtYTdkMi03ZTk4MzQyNDhlMjEiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoicmxuZTlYT2pTWkVQV2h3M241NF9BUkxzTWFqZjFoOFIifQ.wBazjYcMa2fdpReObpq_Hg6w2CgsDZ-1Ja8v6ZexnDr89GI-QQWwofG20r2V6JoZVSBLC7Hwfp1Ba4UrDBtSjnd86lseFxPnK5TV1Y6cewGOijp7-pf7UDho5CfE4sxY";
-            var filename = "smallfile.dat";
+            var url = "<url>";
+            var apiKey = "<apiKey>";
+            var filePath = @"<path to file>";
 
+            var fileName = Path.GetFileName(filePath);
             var restClient = new RestClient(url);
             restClient.AsApiKeyViaQcs(apiKey);
 
             // ********************************************************
             // * Large files must first be uploaded to temp-contents
             // ********************************************************
-            Console.WriteLine($"Uploading file \"{filename}\" to temp-contents... ");
-            var streamContent = new StreamContent(File.OpenRead($@"C:\Tmp\{filename}"));
-            var rsp = restClient.PostHttpAsync($"/api/v1/temp-contents?filename={filename}", streamContent).Result;
+            Console.WriteLine($"Uploading file \"{fileName}\" to temp-contents... ");
+            var streamContent = new StreamContent(File.OpenRead(filePath));
+            var rsp = restClient.PostHttp($"/api/v1/temp-contents?filename={fileName}", streamContent);
             var location = rsp.Headers.Location.ToString();
             Console.WriteLine("File created: " + location);
 
@@ -30,7 +31,7 @@ namespace QcsUploadDataFile
             Console.WriteLine("Importing temporary file to data-files... ");
             var json = JObject.FromObject(new
             {
-                name = filename,
+                name = fileName,
                 tempContentFileId = location.Split('/').Last()
             });
             var content = new MultipartFormDataContent
