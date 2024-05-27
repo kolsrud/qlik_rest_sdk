412848e777a9e2391ff7d09d3f1512e6800c9a90
Øystein Kolsrud <yko@qlik.com>
2024-05-27 08:09:51 +0200

    Cosmetic change.

--- Qlik.Sense.RestClient/Examples/AppUpload/Program.cs
+++ Qlik.Sense.RestClient/Examples/AppUpload/Program.cs
@@ -7,15 +7,15 @@ namespace AppUpload
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-	        var url = "<url>";
-            var restClient = new RestClient(url);
-            restClient.AsNtlmUserViaProxy();
+	    static void Main(string[] args)
+	    {
+		    var url = "<url>";
+		    var restClient = new RestClient(url);
+		    restClient.AsNtlmUserViaProxy();
 
-            var data = File.ReadAllBytes(@"\path\to\app.qvf");
-            const string nameOfApp = "MyUploadedApp";
-            Console.WriteLine(restClient.WithContentType("application/vnd.qlik.sense.app").Post("/qrs/app/upload?keepData=true&name=" + nameOfApp, data));
-        }
+		    var data = File.ReadAllBytes(@"\path\to\app.qvf");
+		    const string nameOfApp = "MyUploadedApp";
+		    Console.WriteLine(restClient.WithContentType("application/vnd.qlik.sense.app").Post("/qrs/app/upload?keepData=true&name=" + nameOfApp, data));
+	    }
     }
 }
