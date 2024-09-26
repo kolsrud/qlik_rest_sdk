6e5431b11dbaa08e1183b6eb1a2bb7bfbdcff5ce
Øystein Kolsrud <yko@qlik.com>
2024-09-26 11:03:32 +0200

    Cosmetic change only.

--- Qlik.Sense.RestClient/ExamplesQcs/QcsExportPdf/Program.cs
+++ Qlik.Sense.RestClient/ExamplesQcs/QcsExportPdf/Program.cs
@@ -6,74 +6,75 @@ using Qlik.Sense.RestClient;
 
 namespace QcsExportPdf
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			var url = "<url>";
-			var apiKey = "<apiKey>";
-			var appId = "<appId>";
-			var objectId = "<objectId>";
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var apiKey = "<apiKey>";
+            var appId = "<appId>";
+            var objectId = "<objectId>";
 
-			var outputFile = "output.pdf";
+            var outputFile = "output.pdf";
 
-			var client = new RestClient(url);
-			client.AsApiKeyViaQcs(apiKey);
+            var client = new RestClient(url);
+            client.AsApiKeyViaQcs(apiKey);
 
-			var requestBody = CreateRequestBody(appId, objectId);
-			var httpRsp = client.PostHttpAsync("/api/v1/reports", requestBody);
-			var httpResult = httpRsp.Result;
-			var statusLocation = httpResult.Headers.Location;
+            var requestBody = CreateRequestBody(appId, objectId);
+            var httpRsp = client.PostHttpAsync("/api/v1/reports", requestBody);
+            var httpResult = httpRsp.Result;
+            var statusLocation = httpResult.Headers.Location;
 
-			Console.WriteLine("Report generation requested. Awaiting process to complete...");
-			var dataLocation = AwaitExportCompletion(client, statusLocation.AbsolutePath);
+            Console.WriteLine("Report generation requested. Awaiting process to complete...");
+            var dataLocation = AwaitExportCompletion(client, statusLocation.AbsolutePath);
 
-			Console.Write("Report generation completed. Downloading exported file... ");
-			var dataLocationUri = new Uri(dataLocation);
-			var bytes = client.GetBytes(dataLocationUri.AbsolutePath);
-			Console.WriteLine("Done!");
+            Console.Write("Report generation completed. Downloading exported file... ");
+            var dataLocationUri = new Uri(dataLocation);
+            var bytes = client.GetBytes(dataLocationUri.AbsolutePath);
+            Console.WriteLine("Done!");
 
-			File.WriteAllBytes(outputFile, bytes);
-			Console.WriteLine($"Wrote {bytes.Length} to file: {outputFile}");
-		}
+            File.WriteAllBytes(outputFile, bytes);
+            Console.WriteLine($"Wrote {bytes.Length} to file: {outputFile}");
+        }
 
-		private static string AwaitExportCompletion(RestClient client, string statusLocation)
-		{
-			var rsp = client.Get<JToken>(statusLocation);
-			while (rsp["status"].Value<string>() != "done")
-			{
-				Console.WriteLine("    Current status: " + rsp["status"]);
-				Thread.Sleep(TimeSpan.FromSeconds(1));
-				rsp = client.Get<JToken>(statusLocation);
-			}
-			Console.WriteLine("    Current status: " + rsp["status"]);
+        private static string AwaitExportCompletion(RestClient client, string statusLocation)
+        {
+            var rsp = client.Get<JToken>(statusLocation);
+            while (rsp["status"].Value<string>() != "done")
+            {
+                Console.WriteLine("    Current status: " + rsp["status"]);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                rsp = client.Get<JToken>(statusLocation);
+            }
 
-			return rsp["results"][0]["location"].Value<string>();
-		}
+            Console.WriteLine("    Current status: " + rsp["status"]);
 
-		private static JToken CreateRequestBody(string appId, string objectId)
-		{
-			var body = new
-			{
-				type = "sense-image-1.0",
-				output = new
-				{
-					outputId = "Chart_pdf",
-					type = "pdf",
-					pdfOutput = new { }
-				},
-				senseImageTemplate = new
-				{
-					appId = appId,
-					visualization = new
-					{
-						id = objectId,
-						widthPx = 613,
-						heightPx = 409,
-					}
-				},
-			};
-			return JToken.FromObject(body);
-		}
-	}
+            return rsp["results"][0]["location"].Value<string>();
+        }
+
+        private static JToken CreateRequestBody(string appId, string objectId)
+        {
+            var body = new
+            {
+                type = "sense-image-1.0",
+                output = new
+                {
+                    outputId = "Chart_pdf",
+                    type = "pdf",
+                    pdfOutput = new { }
+                },
+                senseImageTemplate = new
+                {
+                    appId = appId,
+                    visualization = new
+                    {
+                        id = objectId,
+                        widthPx = 613,
+                        heightPx = 409,
+                    }
+                },
+            };
+            return JToken.FromObject(body);
+        }
+    }
 }
