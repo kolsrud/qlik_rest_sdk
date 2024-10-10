3673a1563f466b96c376cdad8580d4ed2a2a0f8f
Øystein Kolsrud <yko@qlik.com>
2024-10-10 13:35:46 +0200

    Added QCS example AppUpload and removed example ConnectClientCredentials.

---
+++ Qlik.Sense.RestClient/ExamplesQcs/AppUpload/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Qlik.Sense.RestClient;
+
+namespace AppUpload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var apiKey = "<apiKey>";
+            var pathToApp = @"\path\to\app.qvf";
+
+            var client = new RestClientQcs(url);
+            client.AsApiKey(apiKey);
+
+            var appFile = File.ReadAllBytes(pathToApp);
+            var rsp = client.Post<JToken>("/api/v1/apps/import", appFile);
+            Console.WriteLine(rsp);
+        }
+    }
+}
